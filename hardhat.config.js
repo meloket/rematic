95695c3de0451630547fbfe2611bf266464053a1
AtosDev <atos980411@gmail.com>
2022-04-26 18:54:09 -0400

    at pc

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,36 +6,14 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
-const { mnemonic } = require('./secrets.json');
-const { BSC_API_KEY, API_URL_ROPSTEN, API_URL_RINKEBY, PRIVATE_KEY, API_KEY } = process.env;
 
+const { API_URL_ROPSTEN, API_URL_RINKEBY, PRIVATE_KEY, API_KEY } = process.env;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-   defaultNetwork: "testnet",
+   solidity: "0.8.1",
+   defaultNetwork: "rinkeby",
    networks: {
-      localhost: {
-         url: "http://127.0.0.1:8545"
-      },
-      hardhat: {
-      },
-      testnet: {
-         url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-         chainId: 97,
-         gasPrice: 20000000000,
-         accounts: { mnemonic: mnemonic }
-      },
-      mainnet: {
-         url: "https://bsc-dataseed.binance.org/",
-         chainId: 56,
-         gasPrice: 20000000000,
-         accounts: { mnemonic: mnemonic }
-      },
+      hardhat: {},
       ropsten: {
          url: API_URL_ROPSTEN,
          accounts: [`0x${PRIVATE_KEY}`]
@@ -46,30 +24,9 @@ module.exports = {
       }
    },
    etherscan: {
-      // Your API key for Etherscan
-      // Obtain one at https://bscscan.com/
       apiKey: {
          ropsten: API_KEY,
          rinkeby: API_KEY,
-         testnet: BSC_API_KEY,
-         mainnet: BSC_API_KEY
       }
    },
-   solidity: {
-      version: "0.8.0",
-      settings: {
-         optimizer: {
-            enabled: true
-         }
-      }
-   },
-   paths: {
-      sources: "./contracts",
-      tests: "./test",
-      cache: "./cache",
-      artifacts: "./artifacts"
-   },
-   mocha: {
-      timeout: 20000
-   }
-};
+}

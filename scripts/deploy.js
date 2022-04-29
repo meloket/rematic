a8451ad5568bc7bc0c16f7d75ab09682b0bd1997
unknown <cloverdev88@gmail.com>
2022-04-29 13:23:34 -0400

    deploying with external libraries

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,47 @@ const { ethers } = require("hardhat")
 const { upgrades } = require("hardhat")
 
 async function main() {
+  const SafeMath = await ethers.getContractFactory("contracts/Rematic.sol:SafeMath")
 
-  const Rematic = await ethers.getContractFactory("Rematic")
+  // Start deployment, returning a promise that resolves to a contract object
+  const safeMath = await SafeMath.deploy()
+  await safeMath.deployed()
+
+  const IterableMapping = await ethers.getContractFactory("contracts/Rematic.sol:IterableMapping")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const iterableMapping = await IterableMapping.deploy()
+  await iterableMapping.deployed()
+
+  const SafeMathInt = await ethers.getContractFactory("contracts/Rematic.sol:SafeMathInt")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const safMathInt = await SafeMathInt.deploy()
+  await safMathInt.deployed()
+
+  const AddressUpgradeable = await ethers.getContractFactory("contracts/Rematic.sol:AddressUpgradeable")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const addressUpgradeable = await AddressUpgradeable.deploy()
+  await addressUpgradeable.deployed()
+
+  const SafeMathUint = await ethers.getContractFactory("contracts/Rematic.sol:SafeMathUint")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const safeMathUint = await SafeMathUint.deploy()
+  await safeMathUint.deployed()
+
+  const Rematic = await ethers.getContractFactory("Rematic", {
+    libraries: {
+      SafeMath: safeMath.address,
+      IterableMapping: iterableMapping.address,
+      SafeMathInt: safMathInt.address,
+      AddressUpgradeable: addressUpgradeable.address,
+      SafeMathUint: safeMathUint.address,
+    },
+  })
   console.log("Deploying Rematic...")
-  const rematic = await upgrades.deployProxy(Rematic, {initializer: '__Rematic_init'})
+  const rematic = await upgrades.deployProxy(Rematic, {initializer: '__Rematic_init',unsafeAllowLinkedLibraries: true, })
 
   console.log(rematic.address," rematic(proxy) address")
   console.log(await upgrades.erc1967.getImplementationAddress(rematic.address)," getImplementationAddress")
